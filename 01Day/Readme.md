d757996c38c87b8d43886033db6b5cb5469651c3
Abhishek gupta <abhishekspps825406@gmail.com>
2025-11-18 09:23:36 +0530

    Learned about Data types

---
+++ 01Day/Readme.md
@@ -0,0 +1,5 @@
+Day - 01 Learning:
+
+1.Different types of datatypes
+2.Different types of numberic data type
+3.Variables can be declared in one line
---
+++ 01Day/index.js
@@ -0,0 +1,18 @@
+// Declaring variable of different data types
+let firstName = "Abhishek";
+let lastName = "Gupta";
+let Country = "India";
+let city = "Delhi";
+let age = 24;
+let isMarried = false;
+
+// Declaring variables with number values
+let height= 170
+let weight = 71.4
+const gravity = 9.81
+const PI = 3.14
+
+// Variable can also be declared in one line seperated by commas(" , ")
+
+let fName = "Abhishek",job = "Backend Developer" ,live = "Noida" ,waist=32;
+
