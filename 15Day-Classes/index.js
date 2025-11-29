3065e3ebe233361890b1f9f99f73c4c9a4ba928b
Abhishek gupta <abhishekspps825406@gmail.com>
2025-11-29 09:05:47 +0530

    Day 15 completed

--- 15Day-Classes/index.js
+++ 15Day-Classes/index.js
@@ -1,7 +1,41 @@
 // Class
     class Person {
-        // Code goes here
+        // constructor
+        constructor(firstName,lastName,age,country,city) {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.age = age;
+            this.country = country;
+            this.city = city;
+            this.score = 0;
+            this.skills = []
+        }
+        // method
+        // getter
+        get getScore() {
+            return this.score;
+        }
+
+        get getFullName() {
+            return this.firstName + " " + this.lastName;
+        }
+
+        // setter
+        set setScore(score) {
+            this.score += score;
+        }
+        set setSkill(skill) {
+            this.skills.push(skill);
+        }
     }
 
-    const person = new Person();
-    console.log(person)
+    const person1 = new Person("Abhishek" , "Gupta",23,"India","Noida");
+    const person2 = new Person("Ravishek" , "Gupta",23,"India","Noida");
+    const person3 = new Person("Abhilasha" , "Gupta",23,"India","Gurgaon");
+    const person4 = new Person("Piyush rai" , "Gupta",23,"India","Patna");
+    person1.setScore = 243;
+    person1.setSkill = "HTML";
+    person1.setSkill = "Javascro"
+    console.log(person3.city);
+
+    console.log(person1.getFullName,person1.getScore)
